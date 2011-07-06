14a8a3712a86beec9d61194017b41b2d82e21b37
Chris41g <chriswhite@cableone.net>
2011-07-05 19:39:06 -0700

    Edited recovery.c via GitHub

--- recovery.c
+++ recovery.c
@@ -659,20 +659,11 @@ wipe_data(int confirm) {
         }
 
         char* items[] = { " No",
-                          " No",
-                          " No",
-                          " No",
-                          " No",
-                          " No",
-                          " No",
-                          " Yes -- delete all user data",   // [7]
-                          " No",
-                          " No",
-                          " No",
+                          " Yes -- delete all user data",   // [1]
                           NULL };
 
         int chosen_item = get_menu_selection(title_headers, items, 1, 0);
-        if (chosen_item != 7) {
+        if (chosen_item != 1) {
             return;
         }
     }
