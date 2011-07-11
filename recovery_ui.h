d0c600ed2a1597eafe82e9617bd49dc464d23fb8
Chris41g <chriswhite@cableone.net>
2011-07-10 23:44:28 -0700

    Edited recovery_ui.h via GitHub

--- recovery_ui.h
+++ recovery_ui.h
@@ -67,14 +67,14 @@ int device_wipe_data();
 #define GO_BACK             -5
 
 #define ITEM_REBOOT          0
-#define ITEM_APPLY_SDCARD    1
+#define ITEM_POWEROFF        1  
 #define ITEM_WIPE_DATA       2
 #define ITEM_WIPE_CACHE      3
 #define ITEM_INSTALL_ZIP     4
 #define ITEM_NANDROID        5
 #define ITEM_PARTITION       6
 #define ITEM_ADVANCED        7
-#define ITEM_POWEROFF        8          
+        
 
 // Header text to display above the main menu.
 extern char* MENU_HEADERS[];
