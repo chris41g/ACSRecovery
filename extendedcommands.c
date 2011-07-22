4dab554820b00dc529607c873489e8472d4edf1f
Chris41g <chriswhite@cableone.net>
2011-07-22 14:24:41 -0700

    Working on No Confirmation Toggle...

--- extendedcommands.c
+++ extendedcommands.c
@@ -964,13 +964,13 @@ void show_advanced_menu()
                 struct stat info;
                 if (0 == stat("/sdcard/clockworkmod/.no_confirm", &info))
                 {
-                list[6] = "Turn Confirmations Off";
+                list[5] = "Turn Confirmations Off";
                 __system("rm /sdcard/clockworkmod/.no_confirm");
                 break;
                 }
                else
                 {
-                list[6] = "Turn Confirmations Off";
+                list[5] = "Turn Confirmations On";
                 __system("touch /sdcard/clockworkmod/.no_confirm");
                 break;
                 };
